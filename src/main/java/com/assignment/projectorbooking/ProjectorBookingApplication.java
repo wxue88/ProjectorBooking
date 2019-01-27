fb9003b9dab04ce95b16215177aa62996db99ac6
wxue88 <wei.xue@gmail.com>
2019-01-26 20:17:46 -0800

    new

---
+++ src/main/java/com/assignment/projectorbooking/ProjectorBookingApplication.java
@@ -0,0 +1,14 @@
+package com.assignment.projectorbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectorBookingApplication.class, args);
+	}
+
+}
+
