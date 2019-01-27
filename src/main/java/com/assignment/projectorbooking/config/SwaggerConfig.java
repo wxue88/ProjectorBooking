f61a19256ca61a6bb173c4943a52b16f9ccb8fbf
wxue88 <wei.xue@gmail.com>
2019-01-26 21:33:47 -0800

    Add files via upload

--- src/main/java/com/assignment/projectorbooking/config/SwaggerConfig.java
+++ src/main/java/com/assignment/projectorbooking/config/SwaggerConfig.java
@@ -3,9 +3,8 @@ package com.assignment.projectorbooking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.models.Contact;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
